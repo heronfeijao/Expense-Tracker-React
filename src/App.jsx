807e684772d304918d68936c92a51e5718d27c83
Heron Feijao <heronfeijao@gmail.com>
2022-10-03 16:37:20 -0300

    first component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import React from "react";
+import ExpenseItem from "./components/ExpenseItem.jsx";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <p>This is also visible!</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
